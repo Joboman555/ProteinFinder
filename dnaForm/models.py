5af99debcdebded49e908fdee66b0426f6df807b
Jonah Spear <jspear@tableau.com>
2021-01-13 12:15:52 -0700

    add protein_sequence and protein_name to SequenceSearch

--- dnaForm/models.py
+++ dnaForm/models.py
@@ -7,6 +7,8 @@ from Bio import SeqIO
 class SequenceSearch(models.Model):
     sequence = models.CharField(max_length=200)
     search_timestamp = models.DateTimeField()
+    protein_sequence = models.CharField(max_length=1000)
+    protein_name = models.CharField(max_length=20)
 
     def __str__(self):
             return self.sequence
