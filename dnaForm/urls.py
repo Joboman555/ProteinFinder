5dc7f6279d38eb6039a3646b8a4f5a21a8d19369
Jonah Spear <jspear@tableau.com>
2021-01-16 13:23:08 -0800

    Get new results every 0.5 seconds, display as alert (commented out)

--- dnaForm/urls.py
+++ dnaForm/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -6,5 +7,6 @@ app_name = "dnaForm"
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:sequence_search_id/start/', views.start, name='start'),
-    path('<int:sequence_search_id>/', views.detail, name='detail')
+    path('<int:sequence_search_id>/', views.detail, name='detail'),
+    url(r'^ajax/get_results/$', views.getResults, name='get_results'),
 ]
--- dnaForm/views.py
+++ dnaForm/views.py
@@ -4,7 +4,14 @@ from django.template import loader
 from .models import SequenceSearch
 from django.utils import timezone
 from django.urls import reverse
+from django.http import JsonResponse
+from django.core import serializers
 
+# this is called every 0.5 seconds from the frontend to get new results
+def getResults(request):
+    latest_search_list = SequenceSearch.objects.order_by('-search_timestamp')[:5]
+    data = serializers.serialize("json", latest_search_list)
+    return JsonResponse(data, safe=False)
 
 def index(request):
     latest_search_list = SequenceSearch.objects.order_by('-search_timestamp')[:5]
@@ -15,6 +22,7 @@ def index(request):
 def detail(request, sequence_search_id):
     search = get_object_or_404(SequenceSearch, pk=sequence_search_id)
     return render(request, 'dnaForm/detail.html', {'search': search})
+
 
 def start(request):
     try :
@@ -31,8 +39,6 @@ def start(request):
     except Exception as ex:
         latest_search_list = SequenceSearch.objects.order_by('-search_timestamp')[:5]
         context = { 'latest_search_list': latest_search_list,
-                    'error_message': repr(ex)}
+                    'error_message': repr(ex) }
         return render(request, 'dnaForm/index.html', context)
     return redirect('/form/')
-
-
