c103b26f5f87166c4d4dacf2ef9fa7a16ee11fb6
Jonah Spear <jspear@tableau.com>
2021-01-16 16:56:51 -0800

    use env redis stuff

--- ProteinFinder/celery.py
+++ ProteinFinder/celery.py
@@ -11,9 +11,9 @@ app = Celery('ProteinFinder')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-#
-# app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-#                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
 
